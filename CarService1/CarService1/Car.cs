a19ede43f24b4fe7b847f821210e5dbb2fedeb1f
starlord2015 <noreen.ward@rocketmail.com>
2015-04-30 11:39:00 +0100

    CarService

---
+++ CarService1/CarService1/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+using System.Web.Services;
+
+namespace CarService1
+{
+    public class Car 
+    {
+        public String make { get; set; }
+        public String model { get; set; }
+        public String year { get; set; }
+
+      
+    }
+}
